b901eca3dcc117b87a735b43b023d97f0da8d37d
sebas1310 <a20203369@pucp.edu.pe>
2023-04-04 23:18:50 -0500

    ramaw

--- src/main/java/pe/edu/pucp/gtics/lab1221/repository/PlatformsRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.pucp.gtics.lab1221.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.pucp.gtics.lab1221.entity.Platforms;
-
-public interface PlatformsRepository extends JpaRepository<Platforms,Integer> {
-
-
-}
